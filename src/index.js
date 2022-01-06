d41720cdccb7050874560a135c70f6c0459a5b95
Hrvoje Canadjija <hrvoje.canadjija@q.agency>
2022-01-06 11:44:29 +0100

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MordOS from './MordOS';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MordOS />
   </React.StrictMode>,
   document.getElementById('root')
 );
